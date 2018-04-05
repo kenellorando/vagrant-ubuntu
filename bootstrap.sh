acba65555ed3b952130a1f7d7d66ea3b0b8c2686
Ken Ellorando <kenellorando@gmail.com>
2018-04-05 11:12:01 -0500

    Update bootstrap.sh
    Remove git clone

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,5 +4,3 @@ apt-get -y update
 apt-get -y upgrade
 apt-get -f -y install git nodejs postgresql postgresql-contrib
 apt-get -y autoremove
-cd /
-echo 'Run "git clone https://github.com/illinoistech-itm/2018-itmt-7'
